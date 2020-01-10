c7bcac318dac246ff1d32dbb443ae6114b10eada
杨森 <yangsen6313@163.com>
2020-01-10 13:09:42 +0800

    ys

--- LeetCode/BinaryTree.cpp
+++ LeetCode/BinaryTree.cpp
@@ -234,3 +234,114 @@ public:
         return preHead->next;
    }
 };
+/*bottom-to-up算法*/
+class Solution {
+    struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+public:
+    ListNode* sortList(ListNode* head) {
+        ListNode dummyHead(0);
+        dummyHead.next = head;
+        auto p = head;
+        int length = 0;//计算链表长度
+        while (p) {
+            ++length;
+            p = p->next;
+        }
+ 
+        for (int size = 1; size < length; size <<= 1) {
+            auto cur = dummyHead.next;
+            auto tail = &dummyHead;
+ 
+            while (cur) {
+                auto left = cur;
+                auto right = cut(left, size); // left->@->@ right->@->@->@...
+                cur = cut(right, size); // left->@->@ right->@->@  cur->@->...
+ 
+                tail->next = merge(left, right);
+                while (tail->next) {
+                    tail = tail->next;
+                }
+            }
+        }
+        return dummyHead.next;
+    }
+ 
+    ListNode* cut(ListNode* head, int n) {
+        auto p = head;
+        while (--n && p) {
+            p = p->next;
+        }
+ 
+        if (!p) return nullptr;
+ 
+        auto next = p->next;
+        p->next = nullptr;
+        return next;
+    }
+ 
+    ListNode* merge(ListNode* l1, ListNode* l2) {
+        ListNode dummyHead(0);
+        auto p = &dummyHead;
+        while (l1 && l2) {
+            if (l1->val < l2->val) {
+                p->next = l1;
+                p = l1;
+                l1 = l1->next;      
+            } else {
+                p->next = l2;
+                p = l2;
+                l2 = l2->next;
+            }
+        }
+        p->next = l1 ? l1 : l2;
+        return dummyHead.next;
+    }
+};
+
+/*5.insert sort*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+    struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode(int x) : val(x), next(NULL) {}
+    };
+public:
+    ListNode *insertionSortList(ListNode *head) {
+        auto new_head =  head;
+        auto cur = head;
+        ListNode * next;
+        while (next != nullptr)
+        {
+            cur = next;
+            next = cur->next;
+            insert(new_head,cur); 
+        }
+    }
+     ListNode *insert(ListNode *head,ListNode *node)
+     {
+        auto c = head;
+        auto n = head->next;
+        while (c != nullptr)
+        {
+            if (c->val < node->val)
+            {
+                
+            }
+            
+        }
+        
+        
+     }
+};
