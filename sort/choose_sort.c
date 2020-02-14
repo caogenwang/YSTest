13776907f575b7196b10a7bf09cecbbe56431e86
杨森 <yangsen6313@163.com>
2020-02-14 11:24:48 +0800

    ys

--- sort/choose_sort.c
+++ sort/choose_sort.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
 int test[10]={2,9,5,6,4,8,7,3,1,0};
 int main(int argc,char**argv)
 {
@@ -8,8 +10,43 @@ int main(int argc,char**argv)
         printf("%d ",test[i]);
     }
     printf("\n");
-
-    
+    //1、
+    // for (int i = 0; i < 10; i++)
+    // {
+    //     int min = test[i];
+    //     int temp = -1;
+    //     int position = 0;
+    //     for (int j = i+1; j < 10; j++)
+    //     {
+    //         if (test[j] < min)
+    //         {
+    //             min = test[j];
+    //             position = j;
+    //         }
+    //     }
+    //     if (position == 0)
+    //     {
+    //         continue;
+    //     }
+    //     temp = test[i];
+    //     test[i] = test[position];
+    //     test[position] = temp;
+    // }
+    //2、on intenet
+    for (int i = 0; i < 10; i++)
+    {
+        int min = i;
+        for (int j = i+1; j < 10; j++)
+        {
+            if (test[j] < test[min])
+            {
+                min = j;
+            }
+        }
+        int temp = test[i];
+        test[i] = test[min];
+        test[min] = temp;
+    }
     
     printf("The result array:\n");
     for (int i = 0; i < length; i++)
