196c0e08b47418399439acbd770ba09750c2f312
杨森 <yangsen6313@163.com>
2020-02-14 18:22:39 +0800

    ys

--- sort/ShellSort.cpp
+++ sort/ShellSort.cpp
@@ -1,6 +1,40 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+void group_sort(int *a, int n, int i,int gap)
+{
+    int j;
+
+    for (j = i + gap; j < n; j += gap) 
+    {
+        // 如果a[j] < a[j-gap]，则寻找a[j]位置，并将后面数据的位置都后移。
+        if (a[j] < a[j - gap])
+        {
+            int tmp = a[j];
+            int k = j - gap;
+            while (k >= 0 && a[k] > tmp)
+            {
+                a[k + gap] = a[k];
+                k -= gap;
+            }
+            a[k + gap] = tmp;
+        }
+    }
+}
+
+void shellsort(int *array,int length)
+{
+    int gap = length/2;
+    int temp = 0;
+    while(gap>0){
+        for (int i = 0; i < gap; i++)
+        {   
+            group_sort(array,length,i,gap);
+        }
+        gap = gap/2;
+    }
+}
+
 int test[10]={2,9,5,6,4,8,7,3,1,0};
 int main(int argc,char**argv)
 {
@@ -12,6 +46,7 @@ int main(int argc,char**argv)
     }
     printf("\n");
 
+    shellsort(test,10);
 
     printf("The result array:\n");
     for (int i = 0; i < length; i++)
