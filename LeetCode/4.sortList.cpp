adee21dc7b8d162d817e6090f705ae6856223967
杨森 <yangsen6313@163.com>
2020-02-18 10:51:10 +0800

    ys

---
+++ LeetCode/4.sortList.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <string>
+#include <iostream>
+#include <stack>
+#include <map>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+int main(int argc,char**argv)
+{
+    return 0;
+}
