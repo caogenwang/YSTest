357635a1223fa653ebd69d52f0ce876516b96a1d
杨森 <yangsen6313@163.com>
2020-03-14 20:38:21 +0800

    ys

--- LeetCode/19.canCompleteCircuit.cpp
+++ LeetCode/19.canCompleteCircuit.cpp
@@ -60,3 +60,24 @@ public:
         return (total < 0) ? -1 : start;
     }
 };
+
+class Solution3 {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+       int total = 0;
+       int start = 0;
+       int sum = 0;
+       int n = gas.size();
+       for (int i = 0; i < n; i++)
+       {
+           total += gas[i] - cost[i];
+           sum += gas[i] - cost[i];
+           if(sum < 0)
+           {
+               start = i + 1;
+               sum = 0;
+           }
+       }
+       return (total<0)?-1:start;
+    }
+};
