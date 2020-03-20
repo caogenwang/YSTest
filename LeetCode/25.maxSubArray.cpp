38307bae5795f408505415757a687d059c733522
杨森 <yangsen6313@163.com>
2020-03-20 11:20:07 +0800

    ys

--- LeetCode/25.maxSubArray.cpp
+++ LeetCode/25.maxSubArray.cpp
@@ -10,14 +10,49 @@
 #include <limits.h>
 using namespace std;
 
-class Solution {
+/*
+请计算给出的数组（至少含有一个数字）中具有最大和的子数组（子数组要求在原数组中连续
+*/
+class Solution1 {
 public:
     int maxSubArray(int A[], int n) {
-        
+        int res = INT_MIN, curSum = 0;
+        for (int i=0;i<n;i++ ) {
+            int num = A[i];
+            curSum = max(curSum + num, num);
+            res = max(res, curSum);
+        }
+        return res;
+    }
+};
+
+class Solution2 {
+public:
+    int maxSubArray(vector<int>& nums) {
+        if (nums.empty()) return 0;
+        return helper(nums, 0, (int)nums.size() - 1);
+    }
+    int helper(vector<int>& nums, int left, int right) {
+        if (left >= right) return nums[left];
+        int mid = left + (right - left) / 2;
+        int lmax = helper(nums, left, mid - 1);
+        int rmax = helper(nums, mid + 1, right);
+        int mmax = nums[mid], t = mmax;
+        for (int i = mid - 1; i >= left; --i) {
+            t += nums[i];
+            mmax = max(mmax, t);
+        }
+        t = mmax;
+        for (int i = mid + 1; i <= right; ++i) {
+            t += nums[i];
+            mmax = max(mmax, t);
+        }
+        return max(mmax, max(lmax, rmax));
     }
 };
 
 int main(int argc,char**argv)
 {
+    int a[9] = {-2,1,-3,4,-1,2,1,-5,4}; 
     return 0;
 }
