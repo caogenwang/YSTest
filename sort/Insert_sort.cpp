345e2d97196223a3c6050beae06139d4f65de6ad
杨森 <yangsen6313@163.com>
2020-03-30 10:49:11 +0800

    ys

--- sort/Insert_sort.cpp
+++ sort/Insert_sort.cpp
@@ -1,6 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/*
+插入排序的基本思想是：每步将一个待排序的记录，
+按其关键码值的大小插入前面已经排序的文件中适当位置上，直到全部插入完为止
+*/
+int count = 0;
 int main(int argc,char**argv)
 {
     int test[10]={2,9,5,6,4,8,7,3,1,0};
@@ -21,6 +26,7 @@ int main(int argc,char**argv)
         {
             test[i+1] = test[i];
             i--;
+            count ++;
         }
         test[i+1] = temp;
     }
@@ -31,5 +37,6 @@ int main(int argc,char**argv)
         printf("%d ",test[i]);
     }
     printf("\n");
+    printf("count:%d\n",count);
     return 0;
 }
