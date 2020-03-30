2bcf909f5f60b20bb1e47f29483c4c9603e9086f
杨森 <yangsen6313@163.com>
2020-03-30 10:47:29 +0800

    ys

--- sort/choose_sort.c
+++ sort/choose_sort.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-
+#include "display.h"
+/*
+选择排序（Selection sort）是一种简单直观的排序算法。
+它的工作原理是：第一次从待排序的数据元素中选出最小（或最大）的一个元素，
+存放在序列的起始位置，然后再从剩余的未排序元素中寻找到最小（大）元素，
+然后放到已排序的序列的末尾。以此类推，直到全部待排序的数据元素的个数为零。
+选择排序是不稳定的排序方法。
+*/
+int count = 0;
 int test[10]={2,9,5,6,4,8,7,3,1,0};
 int main(int argc,char**argv)
 {
@@ -44,10 +51,12 @@ int main(int argc,char**argv)
             {
                 min = j;
             }
+            count ++;
         }
         int temp = test[i];
         test[i] = test[min];
         test[min] = temp;
+        display(test,10);
     }
     
     printf("The result array:\n");
@@ -56,5 +65,6 @@ int main(int argc,char**argv)
         printf("%d ",test[i]);
     }
     printf("\n");
+    printf("count:%d\n",count);
     return 0;
 }
