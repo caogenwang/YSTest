2baf59c382c297390135ded9a8603e2dfd60fb99
杨森 <yangsen6313@163.com>
2020-07-09 09:35:14 +0800

    ys

--- LeetCode/litter_elephant/4/1.subset.cpp
+++ LeetCode/litter_elephant/4/1.subset.cpp
@@ -29,9 +29,25 @@ public:
         item.pop_back();
         generate(i + 1,S,item,result);
     }
-
-
 };
+
+/*
+1 
+1 2 
+1 2 3 
+1 2 3 4 
+1 2 4 
+1 3 
+1 3 4 
+1 4 
+2 
+2 3 
+2 3 4 
+2 4 
+3 
+3 4 
+4
+*/
 
 class Solution2 {
 public:
@@ -43,7 +59,7 @@ public:
             vector<int> item;
             for (int j = 0; j < S.size(); j++)
             {
-                if (i&(1<<j))
+                if (i&(1<<j))//j的变换代表了存在哪个元素，i的变化代表了每个集合，前提是知道集合的个数
                 {
                     item.push_back(S[j]);
                 }
@@ -53,7 +69,23 @@ public:
         return result;
     }
 };
-
+/*
+1 
+2 
+1 2 
+3 
+1 3 
+2 3 
+1 2 3 
+4 
+1 4 
+2 4 
+1 2 4 
+3 4 
+1 3 4 
+2 3 4 
+1 2 3 4 
+*/
 
 int main(int argc,char**argv)
 {
